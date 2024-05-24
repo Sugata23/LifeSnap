724926e969ce85333e17d48b748b943363a0e133
Sugata <sarkarsugata57@gmail.com>
2024-05-24 20:36:51 +0530

    connecting with mongoDB

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
 app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://sugata21:Passdebona983@cluster0.q17dcsy.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp';
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
-    .then(()=>app.listen(port, ()=>console.log(`Server running on port: ${port}`)))
+    .then(()=>app.listen(PORT, ()=>console.log(`Server running on port: ${PORT}`)))
     .catch((error)=>console.log(error.message));
 //mongoose.set('useFindAndModify', false);
