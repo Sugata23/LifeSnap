ec2695292141ea707f8a9342378573386c1b589d
Sugata <sarkarsugata57@gmail.com>
2024-06-22 00:54:47 +0530

    working on createPost

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,3 +3,5 @@ import axios from 'axios';
 const url = 'http://localhost:5000/posts';
 
 export const fetchPosts = async () => axios.get(url);
+
+export const createPost = async (newPost) => axios.post(url, newPost);
