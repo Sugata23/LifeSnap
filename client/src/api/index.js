49402a3a10d0b64c06a62eb023ded08c742cc165
Sugata <sarkarsugata57@gmail.com>
2024-06-24 12:44:39 +0530

    likepost and deletepost have been done

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -7,3 +7,7 @@ export const fetchPosts = async () => axios.get(url);
 export const createPost = async (newPost) => axios.post(url, newPost);
 
 export const updatePost = async (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+
+export const deletePost = async (id) => axios.delete(`${url}/${id}`);
+
+export const likePost = async (id) => axios.patch(`${url}/${id}/likePost`);
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,7 +5,8 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
 import { useDispatch } from 'react-redux'
-import { useHistory } from 'react-router-dom'
+import { deletePost, likePost } from '../../../actions/posts'
+//import { useHistory } from 'react-router-dom'
 import useStyles from './styles'
 
 const Post = ({ post, setCurrentId }) => {
@@ -30,14 +31,13 @@ const Post = ({ post, setCurrentId }) => {
                 <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" ><ThumbUpAltIcon fontSize="small" /> Like {post.likeCount} </Button>
-                <Button size="small" color="primary" ><DeleteIcon fontSize="small" /> Delete</Button>
+                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))} ><ThumbUpAltIcon fontSize="small" /> &nbsp; Like &nbsp; {post.likeCount} </Button>
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))} ><DeleteIcon fontSize="small" /> Delete</Button>
             </CardActions>
         </Card>
     );
 };
-//onClick={() => dispatch(deletePost(post._id))} 
+
 //onClick={() => history.push(`/posts/${post._id}`)}
-//onClick={() => dispatch(likePost(post._id))}
 
 export default Post
